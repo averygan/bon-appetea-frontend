53694c3becbf265147eb190456980c6e8e258e71
averygdh <avery.gan@foodpanda.com>
2024-12-17 00:50:43 +0800

    added usenavigate for back button instead of redirecting to /

--- src/components/DealsBanner/DealsBanner.jsx
+++ src/components/DealsBanner/DealsBanner.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import { IoArrowBackOutline } from 'react-icons/io5'
 
 function DealsBanner() {
+  const navigate = useNavigate();
+
   return (
     <div className="relative bg-pink-50 py-6 mb-4 px-[3vw] flex items-start">
       {/* Left: back button and title */}
       <div className="flex flex-col items-start gap-3">
-        <Link
-          to="/"
+        <button
+          onClick={() => navigate(-1)}
           className="w-10 h-10 flex items-center justify-center bg-white rounded-full shadow-md"
         >
           <IoArrowBackOutline className="text-2xl text-black" />
-        </Link>
+        </button>
         <h1 className="text-2xl font-bold text-black">Closing Deals</h1>
       </div>
 
