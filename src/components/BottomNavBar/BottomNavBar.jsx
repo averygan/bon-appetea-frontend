3668350c5ea6e7b914ce40e33fba9ef01f8eec94
averygdh <avery.gan@foodpanda.com>
2024-12-16 22:47:46 +0800

    removed comments

--- src/components/BottomNavBar/BottomNavBar.jsx
+++ src/components/BottomNavBar/BottomNavBar.jsx
@@ -5,19 +5,6 @@ import { FaUtensils } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import { BsShop, BsPerson } from "react-icons/bs";
 
-// function BottomNavBar() {
-//   return (
-//       <nav id={styles["bottomNavBar"]}>
-//           <ul>
-//               <li><NavLink to="/" className={({isActive}) => (isActive ? styles.active : undefined)}><FaUtensils/><p>Food</p></NavLink></li>
-//               <li><NavLink to="/grocery" className={({isActive}) => (isActive ? styles.active : undefined)}><BsShop/><p>Grocery</p></NavLink></li>
-//               <li><NavLink to="/search" className={({isActive}) => (isActive ? styles.active : undefined)}><FiSearch/><p>Search</p></NavLink></li>
-//               <li><NavLink to="/account" className={({isActive}) => (isActive ? styles.active : undefined)}><BsPerson/><p>Account</p></NavLink></li>
-//           </ul>
-//       </nav>
-//   )
-// }
-
 function BottomNavBar() {
   const handleClick = (e) => {
     e.preventDefault();  // prevents navigation
