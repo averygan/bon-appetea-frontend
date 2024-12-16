b861321f5e94233cab3e4ae52cd30667adc6d263
averygdh <avery.gan@foodpanda.com>
2024-12-16 23:52:14 +0800

    updated cta on bottombanner and bottombanner bgcolor for dealslistpage

--- src/components/BottomBanner/BottomBanner.jsx
+++ src/components/BottomBanner/BottomBanner.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
-function BottomBanner({text, endpoint, mb = "0px", bgColor="#FFF7FC"}) {
+function BottomBanner({text, endpoint, mb = "0px", bgColor="#FFF7FC", img="paupau-money"}) {
 
   return (
       <Link 
@@ -9,15 +9,14 @@ function BottomBanner({text, endpoint, mb = "0px", bgColor="#FFF7FC"}) {
         className={`flex items-center justify-center py-5 px-8 gap-3 border-t border-gray-300 sticky`}
         style={{ bottom: mb, backgroundColor: bgColor}}>
           <section className="text-left">
-              <h2 className="text-sm">There are currently</h2>
-              <h1 className="text-xl font-bold">over <span className="text-pink-600">25 deals</span> near you!</h1>
-              <p className="text-pink-500 text-sm  mt-2">{text}</p>
+              <h1 className="text-xl font-bold">Save <span className="text-pink-600">50% off</span> closing deals</h1>
+              <p className="text-pink-500 text-sm  font-bold mt-2">{text}</p>
           </section>
           <section className="flex justify-center items-center">
               <img 
-                src="/assets/images/paupau-money.png" 
+                src={`/assets/images/${img}.png`}
                 alt="Pau Pau holding an envelope of cash" 
-                className="w-24 h-auto max-w-full" 
+                className="w-20 h-auto max-w-full" 
               />
           </section>
       </Link>
