400126c28efeafa58e9d594516f0bb3eb0c3ba56
Fiona <128085884+f-lsq@users.noreply.github.com>
2024-12-16 17:06:31 +0800

    Added vendor card

--- src/views/FoodPage.jsx
+++ src/views/FoodPage.jsx
@@ -3,6 +3,7 @@ import HomeBanner from '../components/HomeBanner/HomeBanner'
 import ShopTopBar from '../components/ShopTopBar/ShopTopBar'
 import FilterBar from '../components/FilterBar/FilterBar'
 import BottomNavBar from '../components/BottomNavBar/BottomNavBar'
+import VendorCard from '../components/VendorCard/VendorCard'
 
 function FoodPage() {
   return (
@@ -10,7 +11,7 @@ function FoodPage() {
       <ShopTopBar bgColor="#F70170" locColor="#FFFFFF" searchColor="#FFFFFF"/>
       <HomeBanner/>
       <FilterBar/>
-      <div>Vendor card</div>
+      <VendorCard/>
       <div>Bottom Banner</div>
       <BottomNavBar/>
     </>
