e2832d582f73bc047e1990001edb7c1f9dafdab7
Fiona <128085884+f-lsq@users.noreply.github.com>
2024-12-17 09:52:47 +0800

    updated MapTopBar link and header

--- src/components/MapTopBar/MapTopBar.jsx
+++ src/components/MapTopBar/MapTopBar.jsx
@@ -5,8 +5,8 @@ import { RxCross2 } from 'react-icons/rx'
 function MapTopBar() {
   return (
     <header className="py-[20px] px-[3vw] flex items-center border-b-[1px] border-[#DCDCDC] border-solid">
-        <Link className="text-lg mr-[2vw]" to="/"><RxCross2 /></Link>
-        <h1 className="text-lg font-bold">Closing deals</h1>
+        <Link className="text-lg mr-[2vw]" to="/deals"><RxCross2 /></Link>
+        <h1 className="text-lg font-bold">Closing deals near you</h1>
     </header>
   )
 }
